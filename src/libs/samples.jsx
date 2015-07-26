00a94e85b21c4a408707401263899645cb58615c
Shawn Sandy <shawnsandy04@gmail.com>
2015-07-26 12:36:47 -0400

    Added button to sample component

--- src/libs/samples.jsx
+++ src/libs/samples.jsx
@@ -25,6 +25,7 @@ export default class Component extends React.Component {
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur est fugit, maxime molestias quia quibusdam quidem recusandae reiciendis saepe similique, sit tempore tenetur vel. Accusantium culpa est fuga quae vel.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, quod, velit. Consectetur corporis eos expedita fuga odio sunt vitae voluptates.</p>
                 <p>Aliquid culpa dolor doloribus dolorum, eaque fuga illo inventore magni nemo non nulla obcaecati, quae similique sit tempore veritatis voluptas!</p>
+                <Button name="Component Button" />
 
             </div>
         );
