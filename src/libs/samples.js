c877a8f4f880c5b5d936653ff1e78854b1fd6632
Shawn Sandy <shawnsandy04@gmail.com>
2015-10-12 13:10:08 -0400

    sample statless elements
    Sample Button and Container stateless components

--- src/libs/samples.js
+++ src/libs/samples.js
@@ -13,12 +13,18 @@ import React from 'react';
  * Rect stateless components
  */
 
-const Button = ({
+export const Button = ({
   children = 'Sample Button'
 }) => (
   <button className="btn btn-info">
     {children}
   </button>
+);
+
+export const Container = (props) => (
+  <div clasName="lead">
+    {props.children}
+  </div>
 );
 
 /**
@@ -31,9 +37,15 @@ export default class Component extends React.Component {
 
       <aside className="hello-component">
         <h3>A Sample Component</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur est fugit, maxime molestias </p>
-        <p>Aliquid culpa dolor doloribus dolorum, eaque fuga illo inventore magni nemo non!</p>
-        <Button name="Component Button"/>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur est fugit, maxime molestias. Aliquid culpa dolor doloribus dolorum, eaque fuga illo inventore magni nemo non!</p>
+        <p>
+          <Button children="Component Button"/>
+          </p>
+          <Container>
+            <p className="lead">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem quibusdam excepturi saepe et hic. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, accusantium!
+            </p>
+          </Container>
       </aside>
 
     );
