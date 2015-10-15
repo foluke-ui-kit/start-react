3adc6d845f96ae7efeb360f1fa90d59cba3d2867
Shawn Sandy <shawnsandy04@gmail.com>
2015-10-15 11:13:49 -0400

    Modified new-react-component
    Changed inquirer questions

--- bin/new-react-component.js
+++ bin/new-react-component.js
@@ -1,6 +1,5 @@
 #! /usr/bin/env node
 
-console.log('console output');
 
 var inquirer = require("inquirer");
 
@@ -8,9 +7,16 @@ var questions = [{
   type: "list",
   name: "component_name",
   message: "What type of component would you like to generate",
-  choices: ["ES5", "ES6", "Statless Component", "ES6 Class",  "ES6 Class +"],
+  choices: ["ES5", "ES6", "Statless Component", "ES6 Class", "ES6 Class +"],
   filter: function(val) {
     return val.toLowerCase();
+  }
+}, {
+  type: "input",
+  name: "directory",
+  message: "Please enter the name of the directory, leave blank to use the default",
+  default: function() {
+    return "false";
   }
 }, {
   type: "input",
@@ -20,5 +26,5 @@ var questions = [{
 inquirer.prompt(questions, function(answers) {
   console.log(answers);
   if (answers.name)
-    console.log(answers.name + " -- " + answers.component_name )
+    console.log(answers.name + " -- " + answers.component_name)
 });
